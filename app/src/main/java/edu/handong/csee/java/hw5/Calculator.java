2d474e1e9106d78070243051579d5972c9a2c221
Foryoucom <Foryoucom@DESKTOP-6517BFI>
2023-06-21 16:04:16 +0900

    Made better changes (only sqrt for now)

--- app/src/main/java/edu/handong/csee/java/hw5/Calculator.java
+++ app/src/main/java/edu/handong/csee/java/hw5/Calculator.java
@@ -1,94 +1,109 @@
-package edu.handong.csee.java.hw5;
+	package edu.handong.csee.java.hw5;
+	
+	import edu.handong.csee.java.hw5.engines.*;
+	import edu.handong.csee.java.hw5.exceptions.InvalidCommandException;
+	import edu.handong.csee.java.hw5.clioptions.OptionHandler;
+	import edu.handong.csee.java.hw5.thread.CSVFileCalculator;
+	
+	import java.io.File;
+	import java.util.concurrent.ExecutorService;
+	import java.util.concurrent.Executors;
+	
+	public class Calculator {
+	    public static void main(String[] args) {
+	        Calculator myCalculator = new Calculator();
+	        myCalculator.run(args);
+	    }
+	
+	    public void run(String[] args) {
+	        OptionHandler optionHandler = new OptionHandler();
+	        if (!optionHandler.parseOptions(optionHandler.createOptions(), args)) {
+	            optionHandler.printHelp(optionHandler.createOptions());
+	            return;
+	        }
+	
+	        if (optionHandler.getHelpRequested()) {
+	            optionHandler.printHelp(optionHandler.createOptions());
+	            return;
+	        }
+	
+	        String task = optionHandler.getTask().toUpperCase();
+	
+	        if (task.equals("SQRT")) {
+	            String inputPath = optionHandler.getDataInputFilePath();
+	            String outputPath = optionHandler.getDataOutputFilePath();
 
-import edu.handong.csee.java.hw5.engines.*;
-import edu.handong.csee.java.hw5.exceptions.InvalidCommandException;
-import edu.handong.csee.java.hw5.clioptions.OptionHandler;
-import edu.handong.csee.java.hw5.thread.CSVFileCalculator;
+	            if (inputPath != null && outputPath != null) {
+	                File inputFile = new File(inputPath);
 
-/**
- * This is the Calculator class that is the main class for the user to use.
- */
-public class Calculator {
-    /**
-     * This is the main method of the Calculator class.
-     */
-    public static void main(String[] args) {
-        Calculator myCalculator = new Calculator();
-        myCalculator.run(args);
-    }
+	                if (inputFile.isDirectory()) {
+	                    File[] files = inputFile.listFiles((dir, name) -> name.toLowerCase().endsWith(".csv"));
 
-    /**
-     * This executes the Calculator class for users to use.
-     */
-    public void run(String[] args) {
-        OptionHandler optionHandler = new OptionHandler();
-        if (!optionHandler.parseOptions(optionHandler.createOptions(), args)) {
-            optionHandler.printHelp(optionHandler.createOptions());
-            return;
-        }
+	                    if (files != null && files.length > 0) {
+	                        ExecutorService executor = Executors.newFixedThreadPool(files.length);
 
-        if (optionHandler.getHelpRequested()) {
-            optionHandler.printHelp(optionHandler.createOptions());
-            return;
-        }
+	                        for (File file : files) {
+	                            String outputFilePath = outputPath + File.separator + file.getName();
+	                            CSVFileCalculator calculator = new CSVFileCalculator(file.getAbsolutePath(), outputFilePath);
+	                            executor.execute(calculator);
+	                        }
 
-        String task = optionHandler.getTask().toUpperCase();
+	                        executor.shutdown();
 
-        if (task.equals("SQRT") || task.equals("MIN") || task.equals("MAX")) {
-            String inputFilePath = optionHandler.getDataInputFilePath();
-            String outputFilePath = optionHandler.getDataOutputFilePath();
+	                    } else {
+	                        System.out.println("No CSV files found in the directory: " + inputPath);
+	                    }
+	                } else {
+	                    CSVFileCalculator calculator = new CSVFileCalculator(inputPath, outputPath);
+	                    Thread csvThread = new Thread(calculator);
+	                    csvThread.start();
 
-            if (inputFilePath != null && outputFilePath != null) {
-                CSVFileCalculator calculator = new CSVFileCalculator(inputFilePath, outputFilePath);
-                Thread thread = new Thread(calculator);
-                thread.start();
-                return;
-            }
-        }
-
-        Computable engine = null;
-
-        switch (task) {
-            case "LCM":
-                engine = new LCMEngine();
-                break;
-            case "GCD":
-                engine = new GCDEngine();
-                break;
-            case "SQRT":
-                engine = new SQRTEngine();
-                break;
-            case "FACTORIAL":
-                engine = new FactorialEngine();
-                break;
-            case "FIBONACCI":
-                engine = new FibonacciEngine();
-                break;
-            case "MAX":
-                engine = new MaxEngine();
-                break;
-            case "MIN":
-                engine = new MinEngine();
-                break;
-            case "CUBEVOL":
-                engine = new CubeVolEngine();
-                break;
-            case "SPHEREVOL":
-                engine = new SphereVolEngine();
-                break;
-            default:
-                try {
-                    throw new InvalidCommandException("Please put a computing engine option such as LCM, GCD, SQRT, FACTORIAL, FIBONACCI, MAX, MIN, CUBEVOL, and SPHEREVOL. For example, ./app  MAX 12 4 5 45 100");
-                } catch (InvalidCommandException e) {
-                    System.out.println("Exception-01: Invalid command: " + task);
-                    System.out.println(e.getMessage());
-                    System.exit(0);
-                }
-        }
-
-        engine.setInput(args);
-        engine.compute();
-
-        System.out.println("The result of " + task + " is " + engine.getResult() + ".");
-    }
-}
+	                    String outputFileName = new File(outputPath).getName();
+	                    System.out.println("The " + outputFileName + " file has been successfully written.");
+	                }
+	            }
+	        } else {
+	            Computable engine = null;
+	
+	            switch (task) {
+	                case "LCM":
+	                    engine = new LCMEngine();
+	                    break;
+	                case "GCD":
+	                    engine = new GCDEngine();
+	                    break;
+	                case "FACTORIAL":
+	                    engine = new FactorialEngine();
+	                    break;
+	                case "FIBONACCI":
+	                    engine = new FibonacciEngine();
+	                    break;
+	                case "MAX":
+	                    engine = new MaxEngine();
+	                    break;
+	                case "MIN":
+	                    engine = new MinEngine();
+	                    break;
+	                case "CUBEVOL":
+	                    engine = new CubeVolEngine();
+	                    break;
+	                case "SPHEREVOL":
+	                    engine = new SphereVolEngine();
+	                    break;
+	                default:
+	                    try {
+	                        throw new InvalidCommandException("Please put a valid computing engine option such as LCM, GCD, SQRT, FACTORIAL, FIBONACCI, MAX, MIN, CUBEVOL, or SPHEREVOL.");
+	                    } catch (InvalidCommandException e) {
+	                        System.out.println("Invalid command: " + task);
+	                        System.out.println(e.getMessage());
+	                        System.exit(0);
+	                    }
+	            }
+	
+	            engine.setInput(args);
+	            engine.compute();
+	
+	            System.out.println("The result of " + task + " is " + engine.getResult() + ".");
+	        }
+	    }
+	}
--- app/src/main/java/edu/handong/csee/java/hw5/thread/CSVFileCalculator.java
+++ app/src/main/java/edu/handong/csee/java/hw5/thread/CSVFileCalculator.java
@@ -1,144 +1,96 @@
 package edu.handong.csee.java.hw5.thread;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import java.util.concurrent.TimeUnit;
 
-import org.apache.commons.csv.*;
-
-/**
- * This class implements the Runnable interface and provides methods for reading and writing CSV files,
- * as well as performing calculations for SQRT, MAX, and MIN tasks.
- */
 public class CSVFileCalculator implements Runnable {
     private String inputFilePath;
     private String outputFilePath;
-    private ArrayList<ArrayList<String>> csvData;
 
-    /**
-     * Constructor for CSVFileCalculator class.
-     */
     public CSVFileCalculator(String inputFilePath, String outputFilePath) {
         this.inputFilePath = inputFilePath;
         this.outputFilePath = outputFilePath;
-        this.csvData = new ArrayList<>();
     }
 
-    /**
-     * Reads the CSV file and returns the data as a 2D ArrayList.
-     */
-    public ArrayList<ArrayList<String>> readCSV(String inputFilePath) {
-        try (BufferedReader br = new BufferedReader(new FileReader(inputFilePath))) {
-            String line;
-
-            while ((line = br.readLine()) != null) {
-                ArrayList<String> row = new ArrayList<>();
-                String[] values = line.split(",");
-                for (String value : values) {
-                    row.add(value.trim());
-                }
-                csvData.add(row);
-            }
+    @Override
+    public void run() {
+        try {
+            ArrayList<ArrayList<String>> csvData = readCSV(inputFilePath);
+            calculate(csvData);
+            writeCSV(outputFilePath, csvData);
         } catch (IOException e) {
-            System.out.println("Error occurred while reading the CSV file: " + e.getMessage());
+            e.printStackTrace();
         }
+        System.out.println("Thread finished: " + Thread.currentThread().getName());
+    }
+
+    public ArrayList<ArrayList<String>> readCSV(String filePath) throws IOException {
+        ArrayList<ArrayList<String>> csvData = new ArrayList<>();
+
+        BufferedReader br = new BufferedReader(new FileReader(filePath));
+        String line;
+        while ((line = br.readLine()) != null) {
+            ArrayList<String> row = new ArrayList<>();
+            String[] values = line.split(",");
+            for (String value : values) {
+                row.add(value.trim());
+            }
+            csvData.add(row);
+        }
+        br.close();
 
         return csvData;
     }
 
-    /**
-     * Writes the CSV data to the specified file path.
-     */
-    public void writeCSV(ArrayList<ArrayList<String>> csvData, String outputFilePath) {
-        try (FileWriter writer = new FileWriter(outputFilePath);
-             CSVPrinter csvPrinter = new CSVPrinter(writer, CSVFormat.DEFAULT)) {
+    public void writeCSV(String filePath, ArrayList<ArrayList<String>> csvData) throws IOException {
+        File outputFile = new File(filePath);
 
-            csvPrinter.printRecord(csvData.get(0));
+        outputFile.getParentFile().mkdirs();
 
-            for (int i = 1; i < csvData.size(); i++) {
-                csvPrinter.printRecord(csvData.get(i));
-            }
-        } catch (IOException e) {
-            System.out.println("Error occurred while writing to the CSV file: " + e.getMessage());
+        String inputFileName = new File(inputFilePath).getName();
+        String outputFileName = outputFile.getName();
+
+        String inputFileNameWithoutExtension = inputFileName.substring(0, inputFileName.lastIndexOf("."));
+
+        String outputFileNameWithoutExtension = outputFileName.substring(0, outputFileName.lastIndexOf("."));
+
+        String finalOutputFileName = "";
+
+        if (outputFileNameWithoutExtension.isEmpty()) {
+            finalOutputFileName = inputFileNameWithoutExtension + "-result.csv";
+        } else {
+            finalOutputFileName = outputFileNameWithoutExtension + "-result.csv";
         }
+
+        File finalOutputFile = new File(outputFile.getParentFile(), finalOutputFileName);
+
+        BufferedWriter bw = new BufferedWriter(new FileWriter(finalOutputFile));
+        for (ArrayList<String> row : csvData) {
+            StringBuilder sb = new StringBuilder();
+            for (String value : row) {
+                sb.append(value).append(",");
+            }
+            sb.deleteCharAt(sb.length() - 1);
+            bw.write(sb.toString());
+            bw.newLine();
+        }
+        bw.close();
     }
 
-    /**
-     * Calculates the result for the specified task and input values.
-     */
-    public String calculate(String task, String inputValues) {
-        StringBuilder result = new StringBuilder();
-
-        switch (task) {
-            case "SQRT":
-                result.append("SQRT: ");
-                String[] sqrtValues = inputValues.split(" ");
-                for (String value : sqrtValues) {
+    public void calculate(ArrayList<ArrayList<String>> csvData) {
+        for (int i = 1; i < csvData.size(); i++) {
+            ArrayList<String> row = csvData.get(i);
+            for (int j = 0; j < row.size(); j++) {
+                String value = row.get(j);
+                try {
                     double number = Double.parseDouble(value);
                     double sqrt = Math.sqrt(number);
-                    result.append(sqrt).append(" ");
+                    row.set(j, String.valueOf(sqrt));
+                } catch (NumberFormatException e) {
                 }
-                break;
-            case "MAX":
-                result.append("MAX: ");
-                String[] maxValues = inputValues.split(" ");
-                double max = Double.MIN_VALUE;
-                for (String value : maxValues) {
-                    double number = Double.parseDouble(value);
-                    max = Math.max(max, number);
-                }
-                result.append(max);
-                break;
-            case "MIN":
-                result.append("MIN: ");
-                String[] minValues = inputValues.split(" ");
-                double min = Double.MAX_VALUE;
-                for (String value : minValues) {
-                    double number = Double.parseDouble(value);
-                    min = Math.min(min, number);
-                }
-                result.append(min);
-                break;
-            default:
-                result.append("Invalid task!");
+            }
         }
-
-        return result.toString();
-    }
-
-    /**
-     * The run method that is called when the thread is started.
-     */
-    @Override
-    public void run() {
-        csvData = readCSV(inputFilePath);
-
-        ExecutorService executor = Executors.newFixedThreadPool(10);
-
-        for (int i = 1; i < csvData.size(); i++) {
-            final int rowNumber = i;
-            executor.execute(() -> {
-                String task = csvData.get(rowNumber).get(0);
-                String inputValues = csvData.get(rowNumber).get(1);
-                String result = calculate(task, inputValues);
-                csvData.get(rowNumber).add(result);
-            });
-        }
-
-        executor.shutdown();
-
-        try {
-            executor.awaitTermination(Long.MAX_VALUE, TimeUnit.NANOSECONDS);
-        } catch (InterruptedException e) {
-            System.out.println("Error occurred while waiting for the thread pool to terminate: " + e.getMessage());
-        }
-
-        writeCSV(csvData, outputFilePath);
     }
 }
